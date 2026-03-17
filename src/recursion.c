12a73ccb1b3e4f85406810f3c5bb67b03d0e1e09
Hazal Ertuğrul <ertugrul.hazal@ibu.edu.mk>
2026-03-17 14:38:21 +0300

    Add recursion to print rows based on user input
    Implement recursive function to print rows based on user input.

--- src/recursion.c
+++ src/recursion.c
@@ -1,20 +1,29 @@
-/*
-WEEK 1 — TASK 2 (Recursion)
+#include <cs50.h>
+#include <stdio.h>
 
-Goal:
-Practice recursion.
+void print_rows(int n);
 
-Task:
-1. Ask the user for a positive integer n.
-2. Use a recursive function to print:
+int main(void)
+{
+  int n;
 
-#
-##
-###
-####
+do
+{
+     n = get_int("Height: ");
+}
+while (n < 1);
 
-Rules:
-- You must create a recursive function.
-- You cannot use a loop to print rows.
-- Base case must be correct.
-*/
+print_rows(n);
+}
+
+void print_rows(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+
+    print_rows(n - 1);
+
+    for (int i = 0; i < n; i++)
+    { 
